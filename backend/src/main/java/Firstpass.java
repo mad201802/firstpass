7a6489262dd82de87492ba3ddfdfe44aedbc984c
ll071 <ll071@hdm-stuttgart.de>
2022-10-25 11:20:47 +0200

    Added code to test StrengthAnalyzer

--- backend/src/main/java/Firstpass.java
+++ backend/src/main/java/Firstpass.java
@@ -1,7 +1,16 @@
+import logic.StrengthAnalyzer;
+
 public class Firstpass {
 
     public static void main(String[] args) {
+
+        StrengthAnalyzer analyzer = new StrengthAnalyzer();
+
+        String testPass = "Ddgu463&)/";
+
         System.out.println("Nice!");
+        System.out.println("Your password: " + testPass);
+        System.out.println("Strength: " + analyzer.checkStrength(testPass));
     }
 
 }
