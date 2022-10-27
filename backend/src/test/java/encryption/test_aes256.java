0332780fc63e7fc442b6db7ecaca7c45f5450e40
mad201802 <mad201802@github.io>
2022-10-27 10:32:16 +0200

    #4 working unit tests, added dependencies to pom.xml

--- backend/src/test/java/encryption/test_aes256.java
+++ backend/src/test/java/encryption/test_aes256.java
@@ -2,8 +2,8 @@ package encryption;
 
 import encryption.symmetric.algos.AES256;
 import encryption.symmetric.models.CipherText;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import javax.crypto.Cipher;
 
@@ -14,6 +14,57 @@ public class test_aes256 {
         String text = "hello";
         AES256 aes256 = new AES256("secretsalt", 1000);
         CipherText cipherText = aes256.encryptText(text, "password");
-        Assert.assertNotEquals(text, cipherText.text);
+        String decrypted = aes256.decryptText(cipherText, "password");
+
+        Assertions.assertNotEquals(text, cipherText.text);
+        Assertions.assertEquals(text, decrypted);
     }
+    @Test
+    public void test_encryption_with_wrong_iv_length() {
+        String text = "hello";
+        AES256 aes256 = new AES256("secretsalt", 1000);
+        CipherText cipherText = aes256.encryptText(text, "password");
+        cipherText.iv = "";
+        Exception ex = Assertions.assertThrows(RuntimeException.class, () -> {
+            String decrypted = aes256.decryptText(cipherText, "password");
+        });
+
+        String expectedMessage = "Wrong IV length: must be 16 bytes long";
+        String actualMessage = ex.getMessage();
+
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void test_encryption_with_wrong_iv_content() {
+        String text = "hello";
+        AES256 aes256 = new AES256("secretsalt", 1000);
+        CipherText cipherText = aes256.encryptText(text, "password");
+        cipherText.iv = "jkvTzRDW+rG3m8Y/Wz3F6Q==";
+
+        Exception ex = Assertions.assertThrows(RuntimeException.class, () -> {
+            String decrypted = aes256.decryptText(cipherText, "password");
+        });
+
+        String expectedMessage = "Given final block not properly padded. Such issues can arise if a bad key is used during decryption.";
+        String actualMessage = ex.getMessage();
+
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void test_encryption_with_wrong_password() {
+        String text = "hello";
+        AES256 aes256 = new AES256("secretsalt", 1000);
+        CipherText cipherText = aes256.encryptText(text, "password");
+        Exception ex = Assertions.assertThrows(RuntimeException.class, () -> {
+            String decrypted = aes256.decryptText(cipherText, "not password");
+        });
+
+        String expectedMessage = "Given final block not properly padded. Such issues can arise if a bad key is used during decryption.";
+        String actualMessage = ex.getMessage();
+
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
 }
