95abb7a1287a54bf89caf418e414eeed5cdaa9ee
mad201802 <md148@hdm-stuttgart.de>
2022-11-01 23:16:58 +0100

    #3 resolved merge conflict

--- backend/src/main/java/Firstpass.java
+++ backend/src/main/java/Firstpass.java
@@ -1,33 +1,5 @@
-import database.drivers.SQLiteDriver;
-import logic.StrengthAnalyzer;
+public class FirstPass {
 
-import java.sql.SQLException;
-
-public class Firstpass {
-
-    public static void main(String[] args) throws SQLException {
-
-        StrengthAnalyzer analyzer = new StrengthAnalyzer();
-
-        String testPass = "Ddgu463&)/";
-
-        System.out.println("Nice!");
-        System.out.println("Your password: " + testPass);
-        System.out.println("Strength: " + analyzer.checkStrength(testPass));
-
-        //Driver test/demo
-        SQLiteDriver driver = new SQLiteDriver();
-        driver.deleteAllEntries("Jan Hopsken");
-        driver.addEntry("Jan Hopsken", "SuperCoolesPassword123");
-
-        System.out.println(driver.getEntry("Jan Hopsken").getId());
-        driver.updateEntry(7, "Jan Hopsken", "ASDFGHJKL");
-
-        System.out.println(driver.getAllEntries());
-        System.out.println("Entry: " + driver.getEntry("Jan Hopsken").getUsername() + ", " +
-                driver.getEntry("Jan Hopsken").getPassword());
-
-
-    }
+    public static void main(String[] args) {}
 
 }
