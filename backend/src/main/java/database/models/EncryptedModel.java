76569ddbb34fe1f139fc65d45efe22d94a268b11
mad201802 <md148@hdm-stuttgart.de>
2022-11-02 20:27:10 +0100

    #18 created interface for database access, fixed entry index bug, wrote javadoc

--- backend/src/main/java/database/models/EncryptedModel.java
+++ backend/src/main/java/database/models/EncryptedModel.java
@@ -2,16 +2,33 @@ package database.models;
 
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
+import encryption.symmetric.models.CipherData;
 
+/**
+ * This class represents an encrypted attribute of an entry.
+ */
 @DatabaseTable(tableName = "encrypted")
 public class EncryptedModel {
 
-    @DatabaseField(id = true)
+    @DatabaseField(generatedId = true)
     private Integer id;
-
     @DatabaseField()
     private String cipher;
-
     @DatabaseField()
     private String iv;
+
+    public EncryptedModel() {}
+
+    public EncryptedModel(String cipher, String iv) {
+        this.cipher = cipher;
+        this.iv = iv;
+    }
+
+    public CipherData getCipherData() {
+        CipherData data = new CipherData();
+        data.text = cipher;
+        data.iv = iv;
+        return data;
+    }
+
 }
