b7d9f560ac8d20a3f76aaca768c0541fbfe6f13d
mad201802 <md148@hdm-stuttgart.de>
2022-11-02 11:23:53 +0100

    #3 renamed EncryptedDatabase to EncryptedModel

--- backend/src/main/java/database/drivers/SQLiteDriver.java
+++ backend/src/main/java/database/drivers/SQLiteDriver.java
@@ -4,7 +4,7 @@ import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
-import database.models.EncryptedDatabase;
+import database.models.EncryptedModel;
 import database.models.CategoryModel;
 import database.models.EntryModel;
 
@@ -14,15 +14,15 @@ public class SQLiteDriver {
     //static String url = "jdbc:sqlite:database.db";
     Dao<CategoryModel, Integer> categoryDAO;
     Dao<EntryModel, Integer> entryDAO;
-    Dao<EncryptedDatabase, Integer> encryptedDAO;
+    Dao<EncryptedModel, Integer> encryptedDAO;
     public  SQLiteDriver(String url) throws SQLException {
         ConnectionSource connectionSource = new JdbcConnectionSource(url);
 
         TableUtils.createTableIfNotExists(connectionSource, EntryModel.class);
         entryDAO = DaoManager.createDao(connectionSource, EntryModel.class);
 
-        TableUtils.createTableIfNotExists(connectionSource, EncryptedDatabase.class);
-        encryptedDAO = DaoManager.createDao(connectionSource, EncryptedDatabase.class);
+        TableUtils.createTableIfNotExists(connectionSource, EncryptedModel.class);
+        encryptedDAO = DaoManager.createDao(connectionSource, EncryptedModel.class);
 
         TableUtils.createTableIfNotExists(connectionSource, CategoryModel.class);
         categoryDAO = DaoManager.createDao(connectionSource, CategoryModel.class);
--- backend/src/main/java/database/models/EncryptedModel.java
+++ backend/src/main/java/database/models/EncryptedModel.java
@@ -4,7 +4,7 @@ import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
 @DatabaseTable(tableName = "encrypted")
-public class EncryptedDatabase {
+public class EncryptedModel {
 
     @DatabaseField(id = true)
     private Integer id;
