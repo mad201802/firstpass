54a40ca0dfab9aaf1d4761ea9304f71ae666a53f
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2023-01-05 13:24:00 +0100

    #57 Renamed LoadedDBs to LoadedDB

--- backend/src/main/java/io/firstpass/config/schemas/DefaultConfig.java
+++ backend/src/main/java/io/firstpass/config/schemas/DefaultConfig.java
@@ -3,5 +3,5 @@ package io.firstpass.config.schemas;
 import java.util.ArrayList;
 
 public class DefaultConfig {
-    public ArrayList<LoadedDBs> loadedDBs;
+    public ArrayList<LoadedDB> loadedDBs;
 }
