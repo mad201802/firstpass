1a171ec791408ca3aff9b4beeaa0b25b94c9a714
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2023-01-07 16:32:10 +0100

    #57 Added first unit test

---
+++ backend/src/test/java/io/firstpass/config/TestConfiguration.java
@@ -0,0 +1,40 @@
+package io.firstpass.config;
+
+import io.firstpass.config.schemas.DefaultConfig;
+import io.firstpass.config.schemas.LoadedDB;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class TestConfiguration {
+
+    @Test
+    public void test_loadedDBs() {
+        String currentDirectory;
+        File file = new File("");
+        currentDirectory = file.getAbsolutePath();
+        System.out.println("Current working directory : "+currentDirectory);
+
+
+        DefaultConfig defaultConfig = new DefaultConfig();
+        LoadedDB db = new LoadedDB();
+        db.name = "test_db";
+        db.filepath = "test_filepath";
+        defaultConfig.loadedDBs = new ArrayList<LoadedDB>();
+        defaultConfig.loadedDBs.add(db);
+        Configuration<DefaultConfig> configuration = new Configuration<DefaultConfig>(defaultConfig, currentDirectory + "/fs_test", "testconfig", false);
+        configuration.initConfig();
+        System.out.println("Currently initialized config 1: " +  configuration.getConfig());
+
+        Configuration<DefaultConfig> configuration2 = new Configuration<DefaultConfig>(defaultConfig, currentDirectory + "/fs_test", "testconfig", false);
+        configuration2.initConfig();
+        System.out.println("Currently initialized config 2: " +  configuration2.getConfig());
+
+
+        Assertions.assertEquals(configuration.getConfig().hashCode(), configuration2.getConfig().hashCode());
+        configuration.deleteConfigFile();
+
+    }
+}
