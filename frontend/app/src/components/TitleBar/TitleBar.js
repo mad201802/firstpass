15569b5f5183a0bb48723448e2dad5e9a8fa14cc
nlfmt <nlfmt@gmx.de>
2023-01-15 23:01:56 +0100

    lol 2

--- frontend/app/src/components/TitleBar/TitleBar.js
+++ frontend/app/src/components/TitleBar/TitleBar.js
@@ -9,16 +9,13 @@ const TitleBar = () => {
             <div className="dragqueen"></div>
             <div id="windowControls">
                 <div id="minimizebox" onClick={backend.minimize}>
-                    {" "}
                     <RemoveRounded id="minimize" />
                 </div>
                 <div id="maximizebox" onClick={backend.maximize}>
-                    {" "}
                     <Crop54Rounded id="maximize" />
                 </div>
                 <div id="closebox" onClick={backend.close}>
-                    {" "}
-                    <CloseRounded id="close" />{" "}
+                    <CloseRounded id="close" />
                 </div>
             </div>
         </div>
