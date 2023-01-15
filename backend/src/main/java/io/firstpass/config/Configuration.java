9d372cac0c53defe180f65010d1ce717ce1db771
ll071 <44781682+LeoLTM@users.noreply.github.com>
2023-01-15 22:10:54 +0100

    #57 Added overloaded constructor to be able to create a "Configuration" without specifying a config dir path. Implemented OS detection with automatic path setting. Added javadoc.

--- backend/src/main/java/io/firstpass/config/Configuration.java
+++ backend/src/main/java/io/firstpass/config/Configuration.java
@@ -1,6 +1,8 @@
 package io.firstpass.config;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import fr.hugo4715.oslib.AbstractOperatingSystem;
+import fr.hugo4715.oslib.OperatingSystem;
 import io.firstpass.ipc.exceptions.IPCException;
 
 import java.io.*;
@@ -12,17 +14,50 @@ import java.nio.file.Paths;
 public class Configuration<T> {
     private T config;
     private final String path;
+    private final AbstractOperatingSystem os = OperatingSystem.getOperatingSystem();
     private final String filepath;
     private final Gson gson;
     private final boolean createFolderStructure;
     private final File configFile;
 
+    /**
+     * A program configuration for firstpass.
+     * @param defaultConfiguration
+     * The type the configuration should be based on.
+     * @param path
+     * The path to the directory firstpass should save the config file in.
+     * @param filename
+     * The filename the config file should receive.
+     * @param createFolderStructure
+     * True: Firstpass creates missing parent directories of the specified configuration directory.
+     * False: Firstpass only attempts to create the configuration directory itself.
+     */
     public Configuration(T defaultConfiguration, String path, String filename, Boolean createFolderStructure) {
         this.config = defaultConfiguration;
         this.path = path;
         this.createFolderStructure = createFolderStructure;
         this.gson = new Gson();
-        this.filepath = path + "\\" + filename + ".json";
+        this.filepath = path + File.separator + filename + ".json";
+        this.configFile = new File(this.filepath);
+    }
+
+    /**
+     * A program configuration for firstpass.
+     * Automatically sets configuration directory based on detected operating system.
+     * @param defaultConfiguration
+     * The type the configuration should be based on.
+     * @param filename
+     * The filename the config file should receive
+     * @param createFolderStructure
+     * True: Firstpass creates missing parent directories of the specified configuration directory.
+     * False: Firstpass only attempts to create the configuration directory itself.
+     */
+    public Configuration(T defaultConfiguration, String filename, Boolean createFolderStructure) {
+        this.config = defaultConfiguration;
+        this.path = autoPickPath();
+        this.createFolderStructure = createFolderStructure;
+        this.gson = new Gson();
+        this.filepath = path + File.separator + filename + ".json";
         this.configFile = new File(this.filepath);
     }
 
@@ -104,6 +139,23 @@ public class Configuration<T> {
         // Check if the config file exists.
         Path path = Paths.get(this.filepath);
         return Files.exists(path);
+    }
+
+    /**
+     * Automatically detects the current OS and returns a configuration path.
+     * @return
+     * The configuration path matching the detected OS.
+     */
+    private String autoPickPath() {
+        if(this.os.getType() == OperatingSystem.WINDOWS) {
+            return System.getenv("APPDATA") + "\\firstpass";
+        } else if(this.os.getType() == OperatingSystem.LINUX) {
+            return "~/.config/firstpass";
+        } else if(this.os.getType() == OperatingSystem.OSX) {
+            return "~/.config/firstpass";
+        }
+        System.err.println("OS detection failed! Set path to NULL.");
+        return null;
     }
 
     /**
