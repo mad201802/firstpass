c3c4cf3cddc906153550f64614f68e3bca0751fb
Luca v. K <luca.v.kannen@gmail.com>
2023-01-18 21:40:06 +0100

    Added new requests and respons

---
+++ backend/src/main/java/io/firstpass/ipc/communication/request/UpdateCategoryRequest.java
@@ -0,0 +1,9 @@
+package io.firstpass.ipc.communication.request;
+
+import io.firstpass.utils.decorators.Generated;
+
+@Generated
+public class UpdateCategoryRequest {
+    public int id;
+    public String new_name;
+}
