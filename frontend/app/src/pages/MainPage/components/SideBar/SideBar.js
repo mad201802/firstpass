5a378cf89bd7bb141c8a7fc1f05d65095cdcca25
nlfmt <nlfmt@gmx.de>
2023-01-22 18:17:44 +0100

    better active indicators in sidebar

--- frontend/app/src/pages/MainPage/components/SideBar/SideBar.js
+++ frontend/app/src/pages/MainPage/components/SideBar/SideBar.js
@@ -145,7 +145,7 @@ const SideBar = ({ currentCategory, setCurrentCategory, setSettingsVisible, sett
                     <Category
                         key={category.id}
                         {...category}
-                        currentCategory={currentCategory}
+                        currentCategory={settingsVisible ? -1 : currentCategory}
                         setCurrentCategory={setCurrentCategory}
                         setSettingsVisible={setSettingsVisible}
                         editCategoryVisible={editCategoryVisible}
