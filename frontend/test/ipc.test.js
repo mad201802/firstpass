06e64265d4dee03d7238f95606730c67d114c2f2
mad201802 <md148@hdm-stuttgart.de>
2023-01-29 18:12:43 +0100

    #107 added ipc check

--- frontend/test/ipc.test.js
+++ frontend/test/ipc.test.js
@@ -25,6 +25,7 @@ describe("IPC", () => {
             await ipc.connect();
             expect(ipc.process).toBeDefined();
             expect(ipc.process.pid).toBeDefined();
+            expect(ipc.process.exitCode).toBe(null);
         });
 
         it("should reject with ERR_SPAWN if an error occurs", async () => {
