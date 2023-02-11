2052058098dbed0bcb7f97220fe33f5cee8d976d
nlfmt <nlfmt@gmx.de>
2023-02-11 21:40:33 +0100

    #129 add jsdoc for global components

--- frontend/app/src/components/Popup/Popup.js
+++ frontend/app/src/components/Popup/Popup.js
@@ -5,6 +5,18 @@ import { CloseRounded } from "@mui/icons-material";
 import { Button } from "components";
 import useShortcut from "hooks/useShortcut";
 
+/**
+ * A custom popup
+ * @param {object} props The props
+ * @param {function} props.onClose The function to call when the popup is closed
+ * @param {function} props.onSubmit The function to call when the popup is submitted
+ * @param {string} props.submitText The text of the submit button
+ * @param {string} props.closeText The text of the close button
+ * @param {string} props.title The title of the popup
+ * @param {string} props.type The type of the submit button
+ * @param {string} props.size The size of the popup
+ * @returns The popup component
+ */
 const Popup = ({
     onClose,
     onSubmit,
